6b9763077b2556afe89af6264122958ffd6382b3
Rosu <Georgiana.Rosu@elektrobit.com>
2023-10-24 12:16:32 +0300

    Initial commit

---
+++ src/main/java/ro/uvt/info/laab2/Laab2Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.laab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ro/uvt/info/laab2/Laab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.laab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
